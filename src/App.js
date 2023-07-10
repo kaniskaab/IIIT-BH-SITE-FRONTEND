578e39fce64a07181a16cb965734da556852f45b
sangdil <b121050@iiit-bh.ac.in>
2023-07-10 21:58:33 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Header from './components/header/header';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <Header />
       {/* below div is just to check if the navbar working fine or not */}
       <div className="h-[200vh]"></div>
+      <Footer />
     </>
   );
 }
