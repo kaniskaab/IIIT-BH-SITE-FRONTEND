68fdb2da4f0d4781c3309390419ee4755a6372b1
sangdil <b121050@iiit-bh.ac.in>
2023-08-10 11:25:18 +0530

    cse department section done

--- src/components/DeptCSE/CSE.jsx
+++ src/components/DeptCSE/CSE.jsx
@@ -10,18 +10,40 @@ function CSE() {
   const hod = HodData[0]; // 0 for cse.
   const accordHead1 = 'Department Vision';
   const accordHead2 = 'Department Mission';
+  const accordHead3 = hod.deptScope.title;
   const paraList = hod.deptMission.map((point, index) => (
     <li className="py-2" key={index}>
       {point}
     </li>
   ));
 
+  const { title, para, courses, para2, research, para3 } = hod.deptScope;
+  const scopeContent = (
+    <div>
+      <p>{para}</p>
+      <h3 className="pl-8">Courses:</h3>
+      <ul className="pl-12 list-decimal">
+        {courses.map((course, index) => (
+          <li key={index}>{course}</li>
+        ))}
+      </ul>
+      <p>{para2}</p>
+
+      <ul className="pl-12 list-disc ">
+        {research.map((area, index) => (
+          <li key={index}>{area}</li>
+        ))}
+      </ul>
+      <p>{para3}</p>
+    </div>
+  );
+
   return (
     <>
       <div className="lg:p-[90px] sm:p-12 p-0 "></div>
       <div className="flex flex-col justify-center items-center overflow-x-hidden">
         <ImageSlider slides={SliderData} mainTxt={hod.branch} />;
-        <div className="bg-[#637457] w-full text-center text-[#fffffe] p-8 font-semibold text-2xl">
+        <div className="bg-[#637457] w-full text-center text-[#fffffe] sm:p-8 p-4 font-semibold  text-sm sm:text-2xl">
           Message From Head of the Department
         </div>
         <div className="flex sm:flex-row flex-col sm:gap-12 gap-2">
@@ -53,6 +75,9 @@ function CSE() {
               <Accordion head={accordHead2} para={paraList} />
             </div>
           </div>
+        </div>
+        <div className="my-4">
+          <Accordion head={accordHead3} para={scopeContent} />
         </div>
       </div>
     </>
--- src/components/DeptCSE/data.js
+++ src/components/DeptCSE/data.js
@@ -3,6 +3,8 @@ export const HodData = [
     name: 'Dr. Anjali Mohapatra',
     designation: 'Head of Department',
     branch: 'Computer Science & Engineering',
+    email: 'hod_cse@iiit-bh.ac.in',
+    phone: '+91-674-26533-16',
     aboutPara:
       'Welcome to the Department of Computer Science & Engineering, IIIT Bhubaneswar. Our department takes pride in educating tomorrow’s visionaries, Tech professionals and researchers in frontier areas of computer science. With the support of an established teaching and research framework, it has been possible to establish a strong technical foundation among the students in Computer Science, Engineering and allied frontier research areas. B.Tech students of this department are offered professional and open electives, besides core courses. The thrust is to systematically build up the knowledge base of the students in core areas of computer science followed by rigorous and in-depth learning in individual interest areas. Laboratory subjects, independent/Group projects, and academic research works provide support in mastering required principles and techniques for analysis, design, and experimentation in a variety of fields. The course provides scope for on the job training in projects and assists placement in reputed companies. M.Tech program in the department anchors students toward mastery in areas with significant research opportunities, often oriented towards interdisciplinary areas that take advantage of the tremendous range of faculty expertise of the department. PhD program strongly encourages innovation in research and equip scholars with skills for a successful professional career in technology, teaching, cutting-edge research, consulting and leading a broad range of initiatives. We invite you to be part of our journey as we strive to propel our department of Computer Science & Engineering to ever-greater heights. Sincerely, and with best wishes,',
     deptVision:
@@ -13,5 +15,31 @@ export const HodData = [
       'The Department will cultivate values of honesty and transparency, respect for the Individual, leadership, teamwork, commitment to quality and high standards, passion for performance and sensitivity to social and ethical issues.',
       'To work in collaboration with industries and research establishments in the design and development process to understand the industry practices and diffuse through training.',
     ],
+    deptScope: {
+      title: 'Scope of Computer Science & Engineering',
+      para: 'The Department of Computer Science and Engineering, International Institute of Information Technology Bhubaneswar, was established in 2008, and at present offers the following four years undergraduate programmes, two years Master Degree programmes and Doctoral Degree Programme as follows :',
+      courses: [
+        'B.Tech. degree in Computer Science & Engineering ',
+        'M.Tech degree in Computer Science & Engineering',
+        'Ph.D. degree in Computer Science & Engineering',
+      ],
+      para2:
+        'It has produced high quality technocrats for the last few decades to cater to the needs of hardware and software industry, R&D organisations, and academic Institutions. The department has been accredited for 3 years since 2006 by AICTE with the extension of U.G.C. conferred academic autonomy. The syllabi of the Department are updated with many advanced courses to enable students to keep themselves at par with the cutting edge-technology. The teaching in the department emphasises on fundamental principles, development of creative thinking and the analytical ability to solve real life problems. The Department also encourages its students to engage in extra-curricular and co-curricular activities, personality development, developing team spirit, and developing organisational skills. The Faculty members of the department are involved in diversified advanced research areas like:',
+      research: [
+        'Data Communication and Networking',
+        'Data Mining & Soft Computing',
+        'Distributed Systems',
+        'Soft Computing',
+        'Image and Video Processing',
+        'Internet of Things',
+        'Information Security',
+        'Cloud Computing',
+        'Blockchain',
+        'Bioinformatics',
+        '5G and Advance Wireless Technology',
+      ],
+      para3:
+        'Most of the faculty members have received advanced degrees and/or training from reputed institutions in India. Alumni members of the department hold prestigious positions in leading Software and Hardware companies like Microsoft, IBM, Yahoo, Oracle, Wipro, Infosys, Satyam, TCS, Motorola, Intel, HCL, Samsung, CTS etc in various countries all over the world. Many alumni have been employed in reputed public sector undertakings and academic institutions in India. The department has adequate infrastructures including new laboratories namely Windows NT, UNIX, Computer Hardware, Linux, PC laboratories and one research laboratory for PhD and M.Tech. scholars. The faculty members and students of the department have been honoured at National and International level. The department organises National Technical Seminars, Exhibition, Industry-Institute Interaction Programme every year.',
+    },
   },
 ];
